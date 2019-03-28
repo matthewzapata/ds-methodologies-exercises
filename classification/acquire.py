236fa3f5cf5629e42283fe61fc6137ff4cf44454
Matthew Zapata <matthew.zapata21@gmail.com>
2019-03-28 09:54:33 -0500

    Updated ignore.

--- classification/acquire.py
+++ classification/acquire.py
@@ -3,8 +3,8 @@ import pandas as pd
 def get_titanic_data():
     from env import user, password, host, get_connection
     dbc = get_connection('titanic_db')
-    titanic_df = pd.read_sql('SELECT * FROM passengers', dbc)
-    return titanic_df
+    print('Don\'t forget to assign to a df')
+    return pd.read_sql('SELECT * FROM passengers', dbc)
 
 
 def get_iris_data():
@@ -13,4 +13,5 @@ def get_iris_data():
     measurements = pd.read_sql('SELECT * FROM measurements', dbc)
     names = pd.read_sql('SELECT * FROM species', dbc)
     iris_df = measurements.merge(names, on='species_id')
+    print('Don\'t forget to assign to a df')
     return iris_df
