e640681d6de198b703f485f71b90cef1b69530a2
Matthew Zapata <matthew.zapata21@gmail.com>
2019-03-29 16:15:08 -0500

    Finish explore exercises. Update prepare.py to have working functions for splitting and min-max.

--- classification/prepare.py
+++ classification/prepare.py
@@ -15,6 +15,7 @@ def iris_label_encoder(df):
     species = LabelEncoder()
     species.fit(df.species)
     df['species_encode'] = species.transform(df.species)
+    return species
     
 def prep_iris(df):
     drop_columns(df)
@@ -30,7 +31,7 @@ def titanic_fillna(df):
     print('fill')
     return df
 
-def titanic_drop(df):
+def titanic_drop_deck(df):
     df.drop(['deck'], axis=1, inplace=True)
     print('drop')
     return df
@@ -42,27 +43,26 @@ def titanic_encode_embarked(df):
     print('encode')
     return df
 
-def titanic_split(df):
-    global train, test, X_train, X_test, y_train, y_test, X, y
-    X = df.drop(['passenger_id', 'survived'], axis=1)
-    y = df[['survived']]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.80, random_state=10)
-    train = pd.concat((X_train, y_train), axis=1)
-    test = pd.concat((X_test, y_test), axis=1)
-    print('split')
-    return train
+def split_it(df, training_size=.7, random=123, strat=None):
+    """Train test split. Returns train_df and test_df."""
+    train, test = train_test_split(df, train_size=training_size, random_state=random, stratify=strat)
+    print('Parameters are df, train_size, random_state, and stratify')
+    print('Returns train, test')
+    return train, test
 
-def titanic_min_max_age_fare(train):
+def min_max(train, list_of_what_to_standardize):
+    """Min-max normalization that returns a train_df and the scaler used"""
     scaler = MinMaxScaler()
-    scaler.fit(train[['fare', 'age']])
-    train[['fare', 'age']] = scaler.transform(train[['fare', 'age']])
-    print('standard')
-    return train
+    scaler.fit(train[list_of_what_to_standardize])
+    train[list_of_what_to_standardize] = scaler.transform(train[list_of_what_to_standardize])
+    print('Parameters are a training df and a list of what columns to standardize.')
+    print('Returns the normalized training df and the scaler for scaling the test set.')
+    return train, scaler
     
     
 def prep_titanic(df):
     titanic_fillna(df)
-    titanic_drop(df)
+    titanic_drop_deck(df)
     titanic_encode_embarked(df)
     # titanic_split(df)
     # titanic_min_max_age_fare(train)
